bccc7495536424e00ad87754a89b2f604d04cd59
MxAziz <hereabdulaziz@gmail.com>
2026-04-01 15:54:31 +0600

    added 4 sessional button on 3 semester page

--- src/app/3/page.tsx
+++ src/app/3/page.tsx
@@ -1,8 +1,37 @@
+import Link from "next/link";
+
 export default function Semester3() {
     return (
-        <div>
-            <h1>Semester 3</h1>
-            <p>Content for Semester 3 goes here.</p>
+      <>
+        <div className="h-screen grid grid-cols-2 gap-8 items-place-center p-14 md:p-48 text-center">
+          <Link
+            href="https://www.notion.so/Antenna-Sessional-3348f9f9b287809c922cf0c2341a04bb?source=copy_link"
+            className="px-20 py-16 bg-green-400 rounded-2xl text-black text-2xl"
+          >
+            <button>Analog Code</button>
+          </Link>
+
+          <Link
+            href="https://www.notion.so/Antenna-Sessional-3348f9f9b287809c922cf0c2341a04bb?source=copy_link"
+            className="px-20 py-16 bg-green-400 rounded-2xl text-black text-2xl"
+          >
+            <button>OOP Code</button>
+          </Link>
+
+          <Link
+            href="https://www.notion.so/Antenna-Sessional-3348f9f9b287809c922cf0c2341a04bb?source=copy_link"
+            className="px-20 py-16 bg-green-400 rounded-2xl text-black text-2xl"
+          >
+            <button>TNT Sessional</button>
+          </Link>
+
+          <Link
+            href="https://www.notion.so/Antenna-Sessional-3348f9f9b287809c922cf0c2341a04bb?source=copy_link"
+            className="px-20 py-16 bg-green-400 rounded-2xl text-black text-2xl"
+          >
+            <button>Mathematics Code</button>
+          </Link>
         </div>
+      </>
     );
 }
